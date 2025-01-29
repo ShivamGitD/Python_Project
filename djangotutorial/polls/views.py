e50ba20a0ab11f32e8d10456efe64d3a290e833f
ShivamGitD <shivam.dixit.github@gmail.com>
2025-01-29 23:13:35 +0530

    polls/views.py DetailView get_queryset added

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -34,6 +34,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
@@ -55,3 +61,4 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
